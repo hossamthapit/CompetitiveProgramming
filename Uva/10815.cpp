04deb77fcd8824a4f5221de6cf03ae6c293bb7c5
Hossam Ahmed <25321355+hossamthapit@users.noreply.github.com>
2018-03-10 19:41:58 +0200

    Update 10815.cpp

--- Uva/10815.cpp
+++ Uva/10815.cpp
@@ -1,44 +1,31 @@
-//10815 
-#include <iostream>
 #include <bits/stdc++.h>
-// *__*
-
+/*_*/
 typedef long long ll;
 #define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-const double pi = 3.14159265;
-const ll mod=1000000007;
 
 using namespace std;
-
-string ar[100000];
-
 int main()
-{fast //ll t;cin>>t;while(t--)
-{
-   int c = 0 ;
-   
-   string x ;
-   while(cin >> x)
-    {
-        int sz ;
-        string s;
-        
-        sz = x.size();
+{fast
+    string s ;
+    set <string> st ;
+    while(cin >> s ){
 
-        // clear punctuations
+        string x ;
+        int sz = s.size();
+
         for(int i = 0 ; i < sz;i++)
         {
-            if(x[i]>='A'&&x[i]<='Z')x[i]=tolower(x[i]);
-            if(x[i]>='a'&&x[i]<='z')s+=x[i];
-            else if(s>""){ar[c++]=s;s="";}
+            s[i]=tolower(s[i]);
+            if(s[i]>='a'&&s[i]<='z')x+=s[i];
+            else if(i!=0&&i!=sz-1){st.insert(x);x="";}
         }
-        if(s>"")ar[c++]=s;
+
+        st.insert(x);
+    }
+    set<string>::iterator t ;
+
+    for(t=st.begin();t!=st.end();t++){
+        cout << *t << endl;
     }
 
-    sort(ar,ar+c);
-
-    for(int i = 0 ; i < c ;i++)
-        if(ar[i]!=ar[i+1])cout << ar[i] <<endl;
-
-}
 }
