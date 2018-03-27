43f98f2f8b7a9963ee8d21bb3d1ad4a920e7ffe3
Hossam Ahmed <25321355+hossamthapit@users.noreply.github.com>
2018-03-27 16:48:02 +0200

    Create 11900.cpp

---
+++ Uva/11900.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+/*_*/
+typedef long long ll;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define write  ofstream file ; file.open("HOSSAM.txt");
+#define all(x) x.begin(),x.end()
+
+using namespace std;
+int main()
+{fast write
+
+    int t  ;
+    cin >> t ;
+    for( int y = 1 ; y <= t ; y++ ){
+
+        int n , egg , gram , s = 0 , c = 0 ;
+
+        cin >> n >> egg >> gram ;
+
+        vector < int > v (n);
+
+        for(int i = 0 ; i < n ;i++)
+            cin >> v[i];
+
+        for(int i = 1 ; i <= n ; i++){
+            if( i > egg ) break ;
+            s += v[i-1] ;
+            if( s > gram ) break ;
+            c++ ;
+        }
+       cout << "Case " << y << ": " << c << endl;
+    }
+}
